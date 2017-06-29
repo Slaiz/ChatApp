d6ebd0fe0ca352e9c6b1ff2bb2da2833f9373f96
TEAM\Komarnytskyi <Sviatoslav.Komarnytskyi@neadevis.com>
2017-06-29 16:43:09 +0300

    Created Solution.

---
+++ ChatApp/ChatApp/ChatApp/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Unity;
+using ChatApp.Views;
+using Xamarin.Forms;
+
+namespace ChatApp {
+	public partial class App : PrismApplication {
+		public App ( IPlatformInitializer initializer = null ) : base( initializer ) { }
+
+		protected override void OnInitialized () {
+			InitializeComponent();
+
+			NavigationService.NavigateAsync( "" );
+		}
+
+		protected override void RegisterTypes () {
+		}
+	}
+}
